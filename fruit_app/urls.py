09518e2a85165f10f45aad1482a2c72b9443a81d
Adem <ado.designs@outlook.de>
2025-10-23 07:30:13 +0200

    feat: expand project and created design with templates

--- fruit_app/urls.py
+++ fruit_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import send_fruits_page, single_fruit_view
+from .views import send_fruits_page, single_fruit_view, send_fruits_info_view
 
 urlpatterns = [
     path('', send_fruits_page, name='fruit-list'),
     path('<int:fruit_id>/', single_fruit_view, name='single-fruit'),
+    path('info/', send_fruits_info_view, name='fruit-info'),
 ]
--- fruit_app/views.py
+++ fruit_app/views.py
@@ -6,16 +6,25 @@ import json
 from .fruits_data import fruits
 
 fruits = [
-    {"name": "Apfel", "weight": 100, "color": "red"},
-    {"name": "Banane", "weight": 120, "color": "yellow"},
-    {"name": "Orange", "weight": 150, "color": "orange"},
-    {"name": "Birne", "weight": 130, "color": "green"},
-    {"name": "Kirsche", "weight": 10, "color": "red"}
+    {"name": "Apfel:", "weight": 100, "color": "red",
+        "image": "apfel.jpg", "is_ordered": True},
+    {"name": "Banane:", "weight": 120, "color": "yellow",
+        "image": "banane.jpg", "is_ordered": False},
+    {"name": "Orange:", "weight": 150, "color": "orange",
+        "image": "orange.jpg", "is_ordered": True},
+    {"name": "Birne:", "weight": 130, "color": "green",
+        "image": "birne.jpg", "is_ordered": False},
+    {"name": "Kirsche:", "weight": 10, "color": "red",
+        "image": "kirsche.jpg", "is_ordered": True},
 ]
 
 
 def send_fruits_page(request):
     return render(request, "fruit_app/fruitlist.html", {"fruits": fruits})
+
+
+def send_fruits_info_view(request):
+    return render(request, "fruit_app/info.html")
 
 
 def send_fruits_view(request):
